ef252a90e1546e1c51a5912e4f94e4a63afd7157
skitzoe <jjdelacruz15@gmail.com>
2025-08-09 21:54:08 -0500

    Update addrdb.cpp

--- src/addrdb.cpp
+++ src/addrdb.cpp
@@ -3,6 +3,8 @@
 // Copyright (c) 2017-2019 The Raven Core developers
 // Distributed under the MIT software license, see the accompanying
 // file COPYING or http://www.opensource.org/licenses/mit-license.php.
+#include "clientversion.h"
+
 
 #include "addrdb.h"
 
